8164f75c3dae4528c69122b2aee50001b93ce703
Jagqfeat <Jagqfeat+GitHub@gmail.com>
2018-04-29 14:17:03 -0500

    Functional Emergencies view.
    Will attempt the User screen now.

---
+++ WCrafter/Emergencies.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+class Emergencies: UIViewController {
+
+    // Phone constants
+    let FIREMEN = URL(string: "telprompt://2222322821")
+    let SECURITY = URL(string: "telprompt://911")
+    let AMBULANCE = URL(string: "telprompt://911")
+    
+    //
+    // View Controller Functions
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    //
+    // UI Actions
+    //
+    @IBAction func firemen(_ sender: Any) {
+        self.makeCall(url: FIREMEN!)
+    }
+    @IBAction func security(_ sender: Any) {
+        self.makeCall(url: SECURITY!)
+    }
+    @IBAction func ambulance(_ sender: Any) {
+        self.makeCall(url: AMBULANCE!)
+    }
+    
+    //
+    // Functions
+    //
+    func makeCall(url: URL) {
+        // Command openURL is deprecated since iOS 10.
+        if #available(iOS 10, *) {
+            UIApplication.shared.open(url)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+
+}
---
+++ WCrafter/TabbedViewController.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+class TabbedViewController: UITabBarController {
+
+    //
+    // View Controller Functions
+    //
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //
+    // UI Actions
+    //
+    @IBAction func advanceEmergencies(_ sender: Any) {
+        performSegue(withIdentifier: "tabbedToEmergencies", sender: self)
+    }
+    
+}
