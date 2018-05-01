fd23ffc4156c02720a0bd93ff7b70ef1b0e9606f
Hugo Cruz <HugoCS@macbook-pro-de-hcs.local>
2018-05-01 01:49:02 -0500

    Sòlo se agregaron iconos en pasajeros

---
+++ WCrafter/Destination.swift
@@ -0,0 +1,13 @@
+//
+//  Destination.swift
+//  WCrafter
+//
+//  Created by Hugo Cruz on 01/05/18.
+//  Copyright © 2018 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class Destination: NSObject {
+
+}
